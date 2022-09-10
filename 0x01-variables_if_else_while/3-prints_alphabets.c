7b70b2c1b1efeb512975b68a8c5c7be5ab77cd47
Korode Ebenezer Opeyemi <korode01@gmail.com>
2022-09-10 01:15:01 -0700

    alphBET

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+ char q;
+
+ q = 'a';
+
+ while (q <= 'a')
+{
+
+ putchar(q);
+
+ q++
+}
+
+putchar('\n');
+return(0);
+}
