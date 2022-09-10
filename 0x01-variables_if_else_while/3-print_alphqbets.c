e006e6d5051d37404e498eae1165fbd3613bcf58
Korode Ebenezer Opeyemi <korode01@gmail.com>
2022-09-09 18:07:21 -0700

    3 alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphqbets.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no agument
+ * Return: 0
+ **/
+int main(void)
+{
+ char c;
+ char C;
+
+ for (c = "a';c <= 'z';c++)
+	 putchar(c);
+ for (C = 'A';C <= 'Z';C++)
+	 putchar(C);
+ putchat('\n');
+ return (0);
+}
