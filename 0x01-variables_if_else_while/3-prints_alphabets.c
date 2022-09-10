076c37728398dfe96890d729391c02986b04f995
Korode Ebenezer Opeyemi <korode01@gmail.com>
2022-09-10 01:42:35 -0700

    alphBET

--- 0x01-variables_if_else_while/3-prints_alphabets.c
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -1,26 +1,28 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
-66 * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * main - entry block
+ * @void: no argument
+ * Return: 0 
+ **/
 
 int main(void)
-{
- 
- char q;
 
- q = 'a';
-
- while (q <= 'a')
 {
 
- putchar(q);
+char c;
 
- q++
-}
+char C;
+
+for (c = 'a'; c <= 'z'; c++)
+
+putchar(c);
+
+for (C= 'A'; C <= 'Z'; C++)
+
+putchar(C);
 
 putchar('\n');
+
 return(0);
+
 }
