bd3377173c23973bf5675c0c1acd187ffdbd2a6d
Korode Ebenezer Opeyemi <korode01@gmail.com>
2022-09-11 15:40:20 -0700

    smile in the mirrow

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry block
+ 
+ * description - print the lowercase alphabet in reverse
+ 
+Return: 0
+
+**/
+
+int main(void)
+
+{
+
+ char c;
+
+ for (c = 'z' ; c >= 'a' c--)
+
+  putchar(c);
+
+ putchat('\n');
+
+ Return (0);
+
+}
